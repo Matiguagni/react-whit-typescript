fab567fece37d0dce94e2e5f544301da437f64c4
Matias Guagni <matug2008@hotmail.com>
2024-02-28 11:12:45 -0300

    Commit carpeta de componentes y componente RandomFox

---
+++ components/RandomFox.tsx
@@ -0,0 +1,9 @@
+
+
+type Props = {image:string};
+
+export const RandomFox = ({image}:Props): JSX.Element => {
+    
+
+    return <img width={320} height="auto" src={image} className="rounded" />
+}
